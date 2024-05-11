8d348c9db320ae420ab846b52b1140d424a91581
Myeongjeon Lee <userclassdev@outlook.com>
2024-05-11 15:17:50 -0500

    basic JSX structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,24 @@
-import React from 'react';
+import { useState } from 'react';
+import './css/styles.css';
+import './css/reset.css';
 
 function App() {
+  const [newItem, setNewItem] = useState('');
+
   return (
     <div>
-      {/* Your app content goes here */}
+      <h1>To-do list app</h1>
+      <input
+        type="text"
+        placeholder="Add an item"
+        value = {newItem}
+      />
+      <button>Add</button>
+      <ul>
+        <li>take out the trash</li>
+        <li>water the plants</li>
+        <li>go to the gym</li>
+      </ul>
     </div>
   );
 }
