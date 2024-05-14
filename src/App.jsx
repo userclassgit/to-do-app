020daf8f6956d447ddbee83459c8672106a36254
Myeongjeon Lee <userclassdev@outlook.com>
2024-05-14 18:54:38 -0500

    add add button icon

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import TodoItem from './component/TodoItems';
 import './css/styles.css';
 import './css/reset.css';
 import { v4 as uuidv4 } from 'uuid';
+import addBtnImg from './media/add-btn.png'
 
 const initialState = {
   newItem: '',
@@ -66,7 +67,7 @@ function App() {
           onChange={(e) => dispatch({ type: 'set_new_item', payload: e.target.value })}
         />
         {!state.isValid && <p className='error-message'>Please enter a value.</p>}
-        <button onClick={() => addItem()}>Add</button>
+        <button className='add-btn' onClick={() => addItem()}></button>
       </div>
       <ul>
         <ul>
